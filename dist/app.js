c858e56c721724e84d98f69520bf4791d6039a6f
shubham vora <shubhamvora05@gmail.com>
2022-03-30 07:53:42 +0530

    heroku

--- dist/app.js
+++ dist/app.js
@@ -2,12 +2,18 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+
+//path = require('path');
+
+//app.use(express.static(path.join(__dirname)));
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+
+var port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.sendFile('home.html', {root: __dirname })
+    res.sendFile(__dirname+"/home.html");
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
